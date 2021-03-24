99eabf14f4b6d69fcfac6518464e8d8860a3e5a8
sid124557 <sid124557@gmail.com>
2021-03-24 22:58:51 +0530

    nskjndcskjnd

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {"data":{"name":"siddharth"}}
+@app.get('/blog')
+def index(sort: Optional [str], limit=10, published: bool = True  ):
+    if published == True:
+        return {"data": f" {limit} published blog from db"}
+    else:
+        return {"data": f" {limit} blog from db"}
+
+
+@app.get("/blog/unpublished")
+def unpublished_blog():
+    return {'data': 'all unpublished blog'}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": {id}}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': ('1', '2', '3')}
